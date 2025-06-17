44c7d4d78acbfe0d5c6245838b77b2b94374ad9f
JozefB-rgb <jozefBr6@gmail.com>
2025-06-17 20:36:16 +0100

    RESTRUCTURIZATION: Add testIPStructires executable - saparate test for classes from testIPStructus from other tests to make code clearer. Adding required files and modify cmake.

--- clockMachineSimulator/tests/test_main.cpp
+++ clockMachineSimulator/tests/test_main.cpp
@@ -5,7 +5,6 @@
 #include <string>
 
 #include "TimeSynchronizator.h"
-#include "IPStructures.h"
 
 class TimeSynchronizatorTest :public ::testing::Test
 {
@@ -144,42 +143,6 @@ TEST_F(TimeSynchronizatorTest, testTimeStructureThenMakeString) {
 	obj.updateTime();
 
 	EXPECT_EQ(obj.getTime(), "1996-12-08 18:00:08.014052");
-}
-
-//test whole process of loading IP, ports and local port adress form file 
-TEST_F(TimeSynchronizatorTest, testLoadingIPFromFileProcess) {
-	OwnPort ownPort("321");
-	AdressStructure firstDevice = {
-		.IP = "195.128.0.1",
-		.port = "8008"
-	};
-	AdressStructure secondDevice = {
-		.IP = "295.128.0.1",
-		.port = "12024"
-	};
-	AdressStructure thirdDevice = {
-		.IP = "5.1523.42.1",
-		.port = "12024"
-	};
-
-	//loading IP and ports of remote devices from file to adress structure
-	std::string IPFilePath = "";
-	IPFileReader fileReader;
-	RemoteDeviceList remoteDevices;
-	OwnPort ownPortFromFile;
-	fileReader.loadAdresses(IPFilePath, remoteDevices, ownPortFromFile);
-
-	//checks if local port adress match
-	EXPECT_EQ(ownPort.get(), ownPortFromFile.get());
-
-	//checks if firstDevice form test and form file match
-	EXPECT_EQ(firstDevice, remoteDevices.getDevice(0));
-
-	//checks if secondDevice IP from test match second IP from file
-	EXPECT_EQ(secondDevice.IP, remoteDevices.getIP(1));
-
-	//checks if thirdDevice port from test match third port from file
-	EXPECT_EQ(thirdDevice.port, remoteDevices.getPort(2));
 }
 
 //test almost the whole module
