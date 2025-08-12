6633435bf5438a115e4c7e72a933e063da0e7e14
JozefB-rgb <jozefBr6@gmail.com>
2025-08-12 03:22:04 +0100

    Moved ip class methods form .h file to .cpp file

--- clockMachineSimulator/src/IPStructures.cpp
+++ clockMachineSimulator/src/IPStructures.cpp
@@ -10,6 +10,38 @@ OwnPort::OwnPort() : port_(NULL) {};
 OwnPort::OwnPort(std::string port) : port_(port) {}
 std::string OwnPort::get() { return port_; };
 
+//class IP
+std::string IP::checkIP(std::string ip)
+{
+	size_t dotPossition;
+	size_t prevoiusDotPossition;
+	std::string ipBlock;
+	int i_ipBlock;
+
+	dotPossition = 0;
+	prevoiusDotPossition = -1;
+	for (int block = 1; block <= 4; block++)
+	{
+		dotPossition = ip.find(".", dotPossition + 1);
+		if ((dotPossition == std::string::npos && (block != 4)))
+			throw std::invalid_argument("IP is not ***.***.***.*** format");
+		else if (block == 4)
+			ipBlock = ip.substr(prevoiusDotPossition + 1);
+		else
+			ipBlock = ip.substr(prevoiusDotPossition + 1, dotPossition - prevoiusDotPossition - 1);
+		{
+			size_t pos;
+			i_ipBlock = std::stoi(ipBlock, &pos);
+			if (pos != ipBlock.size())
+				throw std::invalid_argument("Not only digits inside IP");
+		}
+		if (!(0 <= i_ipBlock && i_ipBlock <= 255))
+			throw std::invalid_argument("Invalid Ip block");
+		prevoiusDotPossition = dotPossition;
+	}
+	return ip;
+}
+
 //class RemoteDeviceList
 AdressStructure RemoteDeviceList::getDevice(int pos) { return devices_.at(pos); };
 std::string RemoteDeviceList::getIP(int pos) { return devices_.at(pos).IP; };
--- clockMachineSimulator/src/IPStructures.h
+++ clockMachineSimulator/src/IPStructures.h
@@ -21,36 +21,7 @@ class IP
 {
 	std::string ip_;
 
-	std::string checkIP(std::string ip)
-	{
-		size_t dotPossition;
-		size_t prevoiusDotPossition;
-		std::string ipBlock;
-		int i_ipBlock;
-
-		dotPossition = 0;
-		prevoiusDotPossition = -1;
-		for (int block = 1; block <= 4; block++)
-		{
-			dotPossition = ip.find(".", dotPossition + 1);
-			if ((dotPossition == std::string::npos && (block != 4)))
-				throw std::invalid_argument("IP is not ***.***.***.*** format");
-			else if (block == 4)
-				ipBlock = ip.substr(prevoiusDotPossition + 1);
-			else
-				ipBlock = ip.substr(prevoiusDotPossition + 1, dotPossition - prevoiusDotPossition - 1);
-			{
-				size_t pos;
-				i_ipBlock = std::stoi(ipBlock, &pos);
-				if (pos != ipBlock.size())
-					throw std::invalid_argument("Not only digits inside IP");
-			}
-			if (!(0 <= i_ipBlock && i_ipBlock <= 255))
-				throw std::invalid_argument("Invalid Ip block");
-			prevoiusDotPossition = dotPossition;
-		}
-		return ip;
-	}
+	std::string checkIP(std::string ip);
 public:
 	IP(const std::string& ip) : ip_(checkIP(ip)) {}
 	const std::string& getIP() const { return ip_; };
